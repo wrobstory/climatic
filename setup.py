ff7ddece5830befa4d52ac50a02bb16e47104ba5
Rob Story <wrobstory@gmail.com>
2013-02-20 20:47:56 -0800

    Added setup.py and .gitignore update

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from os.path import abspath, dirname, join
+
+path = abspath(dirname(__file__))
+
+classifiers = (
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'License :: OSI Approved :: MIT License',
+)
+
+requires = (
+    'matplotlib', 
+    'numpy',
+)
+
+kw = {
+    'name'                 : 'climatic',
+    'version'              : '0.1.0',
+    'description'          : 'A small toolbox of wind data plotting tools',
+    'long_description'     : open(join(path, 'README.rst')).read(),
+    'author'               : 'Rob Story',
+    'author_email'         : 'wrobstory@gmail.com',
+    'license'              : 'MIT License',
+    'url'                  : 'https://github.com/wrobstory/climatic',
+    'keywords'             : 'wind data plotting',
+    'classifiers'          : classifiers,
+    'modules'              : ['climatic'],
+    'install_requires'     : requires, 
+    'zip_safe'             : True,
+}
+
+setup(**kw)
