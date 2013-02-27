a6af5c0260ab3ab60be83c88463be682280de2ed
Rob Story <wrobstory@gmail.com>
2013-02-26 21:07:31 -0800

    Test update

--- tests/test_mast.py
+++ tests/test_mast.py
@@ -6,9 +6,12 @@ class TestMast(unittest.TestCase):
 
     def test_latlon(self):
         #Test for standard input
-        test_mast = MetMast(lat=45.5236, lon=122.6750)
+        test_mast = MetMast(lat=45.5236, lon=122.6750, height=80, 
+                            time_zone='US/Eastern')
         self.assertEqual(test_mast.lat, 45.5236)
         self.assertEqual(test_mast.lon, 122.6750)
+        self.assertEqual(test_mast.height, 80)
+        self.assertEqual(test_mast.time_zone, 'US/Eastern')
                 
 def main():
     unittest.main()
