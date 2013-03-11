1d9161e4384d06163d50a3a47cf0cf01f65aab3a
Rob Story <wrobstory@gmail.com>
2013-03-10 22:18:34 -0700

    Added start of weibull method to mast.py, as well as tests for method

--- tests/test_mast.py
+++ tests/test_mast.py
@@ -1,5 +1,8 @@
+from __future__ import print_function
 import unittest
+import os
 import pandas as pd
+import numpy as np
 from climatic import MetMast
 from pandas.util.testing import assert_almost_equal
 
@@ -19,12 +22,35 @@ class TestMast(unittest.TestCase):
         pandas testing suite'''
         test_mast = MetMast()
         col_names = ['Wind Speed 1', 'Std Dev 1', 'Wind Direction 1']
-        true_df = pd.read_table(r'test_data_import.csv', header=0,
+        pkg_dir, filename = os.path.split(os.path.abspath(__file__))
+        test_import = os.path.join(pkg_dir, r'data/test_data_import.csv')
+        true_df = pd.read_table(test_import, header=0,
                                 index_col=0,delimiter=',',
                                 names=col_names)
-        test_mast.wind_import(r'test_data_import.csv', header_row=0, 
+        test_mast.wind_import(test_import, header_row=0, 
                               time_col=0, delimiter=',', names=col_names)
         assert_almost_equal(true_df, test_mast.data)
+        
+    def test_weibull(self):
+        '''Test the weibull generating method'''
+        test_mast = MetMast()
+        col_names = ['Wind Speed 1', 'Std Dev 1', 'Wind Direction 1']
+        pkg_dir, filename = os.path.split(os.path.abspath(__file__))
+        test_import = os.path.join(pkg_dir, 
+                                   r'data/USDOE_beresford_051201.csv')
+        true_df = pd.read_table(test_import, header=54,
+                                index_col=0,delimiter=',',
+                                names=col_names)
+        cut_bins = np.arange(0,true_df['Wind Speed 1'].max()+1,1)
+        binned = pd.cut(true_df['Wind Speed 1'], cut_bins)
+        dist_10min = pd.value_counts(binned).reindex(binned.levels)
+        dist = pd.DataFrame({'Binned: 10 Minute': dist_10min})
+        dist['Binned: Hourly'] = dist['Binned: 10 Minute']/6
+        dist = dist.fillna(0)
+        test_mast.wind_import(test_import, header_row=54, 
+                              time_col=0, delimiter=',', names=col_names)
+        assert_almost_equal(dist, test_mast.weibull(column='Wind Speed 1'))
+        
                 
 def main():
     unittest.main()
